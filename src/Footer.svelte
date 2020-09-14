55e771ed003d8bc1fd15f2dbc45fe49908182ae5
Jens de Rond <jensderond@gmail.com>
2020-09-14 23:40:21 +0200

    fix security issue on link

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,3 +1,3 @@
 <div class="footer whitespace-normal text-xs text-center mt-6">
-    <i><strong>This project is open source on <a href="https://github.com/EdinTC/Checq" target="_blank">Github</a>, feel free to support us!</strong></i>
+    <i><strong>This project is open source on <a href="https://github.com/EdinTC/Checq" rel="noopener" target="_blank">Github</a>, feel free to support us!</strong></i>
 </div>
